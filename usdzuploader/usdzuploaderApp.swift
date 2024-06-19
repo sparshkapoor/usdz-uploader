d74808e0a8bc5f109c906189e77171a3fe34fd18
Sparsh Kapoor <kapoor.sparsh@outlook.com>
2024-06-19 16:36:22 -0400

    Initial Commit

---
+++ usdzuploader/usdzuploaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  usdzuploaderApp.swift
+//  usdzuploader
+//
+//  Created by WorkMerkDev on 6/19/24.
+//
+
+import SwiftUI
+
+@main
+struct usdzuploaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
