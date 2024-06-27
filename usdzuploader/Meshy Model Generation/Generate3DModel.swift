b715f5a661a0384201740cb0b0905912ebbeabfa
Sparsh Kapoor <kapoor.sparsh@outlook.com>
2024-06-27 12:36:38 -0400

    model generation with preview via camera

---
+++ usdzuploader/Meshy Model Generation/Generate3DModel.swift
@@ -0,0 +1,8 @@
+//
+//  Generate3DModel.swift
+//  usdzuploader
+//
+//  Created by WorkMerkDev on 6/27/24.
+//
+
+import Foundation
---
+++ usdzuploader/Meshy Model Generation/MeshyAPI.swift
@@ -0,0 +1,8 @@
+//
+//  MeshyAPI.swift
+//  usdzuploader
+//
+//  Created by WorkMerkDev on 6/27/24.
+//
+
+import Foundation
